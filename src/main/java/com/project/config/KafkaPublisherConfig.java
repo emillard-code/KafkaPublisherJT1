19c7290475c78d151152e9a869c2a60f26181f19
Emerson Millard <emillard.code@gmail.com>
2022-07-28 12:00:00 -0700

    Modified configuration class.

--- src/main/java/com/project/config/KafkaPublisherConfig.java
+++ src/main/java/com/project/config/KafkaPublisherConfig.java
@@ -17,16 +17,20 @@ public class KafkaPublisherConfig {
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
+
         Map<String, Object> configs = new HashMap<>();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<String, Object>(configs);
+
     }
 
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
+
         return new KafkaTemplate<>(producerFactory());
+
     }
 
 }
