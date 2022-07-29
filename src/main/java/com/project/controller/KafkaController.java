c13b0a52d881db6edc01569fc5c7d9efff2bfce1
Emerson Millard <emillard.code@gmail.com>
2022-07-29 12:00:00 -0700

    Added controller methods.

--- src/main/java/com/project/controller/KafkaController.java
+++ src/main/java/com/project/controller/KafkaController.java
@@ -1,4 +1,31 @@
 package com.project.controller;
 
+import com.project.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KafkaController {
+
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    private String topic = "javatechie";
+
+    @GetMapping("/publish/{name}")
+    public String publishMessage(@PathVariable String name) {
+        template.send(topic, "Hi " + name + " Welcome to java techie");
+        return "Data published";
+    }
+
+    @GetMapping("/publishJson")
+    public String publishMessage() {
+        User user = new User(2532, "User88", new String[] { "Bangalore", "BTM", "house 90" });
+        template.send(topic, user);
+        return "Json Data published";
+    }
+    
 }
