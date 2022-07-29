28fcdd197a88a9473260cb3fd63610babbabee9e
Emerson Millard <emillard.code@gmail.com>
2022-07-29 12:00:00 -0700

    Removed unnecessary imports.

--- src/main/java/com/project/KafkaPublisherJt1Application.java
+++ src/main/java/com/project/KafkaPublisherJt1Application.java
@@ -1,13 +1,7 @@
 package com.project;
 
-import com.project.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class KafkaPublisherJt1Application {
