d2897173bb74083baed02cc41e1802d47593185b
Emerson Millard <emillard.code@gmail.com>
2022-07-29 12:00:00 -0700

    Moved methods to controller class.

--- src/main/java/com/project/KafkaPublisherJt1Application.java
+++ src/main/java/com/project/KafkaPublisherJt1Application.java
@@ -13,24 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KafkaPublisherJt1Application {
 
-    @Autowired
-    private KafkaTemplate<String, Object> template;
-
-    private String topic = "javatechie";
-
-    @GetMapping("/publish/{name}")
-    public String publishMessage(@PathVariable String name) {
-        template.send(topic, "Hi " + name + " Welcome to java techie");
-        return "Data published";
-    }
-
-    @GetMapping("/publishJson")
-    public String publishMessage() {
-        User user = new User(2532, "User88", new String[] { "Bangalore", "BTM", "house 90" });
-        template.send(topic, user);
-        return "Json Data published";
-    }
-
     public static void main(String[] args) {
 
         SpringApplication.run(KafkaPublisherJt1Application.class, args);
