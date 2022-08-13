0c4a8a23bbc224c5f2b03df5b6c689411ba75630
Emerson Millard <emillard.code@gmail.com>
2022-08-13 12:00:00 -0700

    Added Kafka content to main class.

--- src/main/java/com/project/KafkaPublisherJt1Application.java
+++ src/main/java/com/project/KafkaPublisherJt1Application.java
@@ -1,13 +1,32 @@
-package com.project.kafkapublisherjt1;
+package com.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class KafkaPublisherJt1Application {
 
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    private String topic = "javatechie";
+
+    @GetMapping("/publish/{name}")
+    public String publishMessage(@PathVariable String name) {
+        template.send(topic, "Hi " + name + " Welcome to java techie");
+        return "Data published";
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(KafkaPublisherJt1Application.class, args);
+
     }
 
 }
