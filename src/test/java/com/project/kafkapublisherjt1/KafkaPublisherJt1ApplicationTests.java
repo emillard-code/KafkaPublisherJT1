318a99de2db196087e151ee5fc94154b604c01c3
Emerson Millard <emillard.code@gmail.com>
2022-08-18 16:56:43 -0700

    Initial commit

---
+++ src/test/java/com/project/kafkapublisherjt1/KafkaPublisherJt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kafkapublisherjt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPublisherJt1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
